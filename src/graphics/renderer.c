f73daa0d2f10b47612afe649efda7227353e48e3
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-08-07 08:54:41 +0200

    Created the first function which is essentially the setup of the renderer, gonna test it later

--- src/graphics/renderer.c
+++ src/graphics/renderer.c
@@ -5,5 +5,64 @@
 
 #include "renderer.h"
 
-// Implementation will go here
+#ifdef __APPLE__
+#include <OpenGL/gl.h>
+#else 
+#include <GL/gl.h>
+#endif
+
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+static GLFWwindow *window = NULL;
+static bool wireframe_enabled = false;
+
+int init_renderer(int width, int height, const char *title) {  
+
+
+    #ifdef __APPLE__
+        /* MacOS M chips specific */
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+        glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+    #elif _WIN32
+        /* Windows: Can go up to the latest version supported by drivers */
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    #else
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+        glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    #endif
+
+    GLFWmonitor *primary = glfwGetPrimaryMonitor();
+
+    glfwInit();
+
+    window = glfwCreateWindow(width, height, title, primary, NULL);
+
+    if (!window) {
+        glfwTerminate();
+        return (-1);
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glEnable(GL_DEPTH_TEST);
+
+    glViewport(0, 0, width, height);
+
+    glEnable(GL_CULL_FACE);
+
+    printf("OpenGL Version: %s\n",
+            glGetString(GL_VERSION));
+
+    return (0);
+}   
+
+
+
 
