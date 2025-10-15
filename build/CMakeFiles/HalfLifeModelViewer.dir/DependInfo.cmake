2a3ed91459320e62ed9f073d3b5fc9953c490746
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-10-15 10:13:32 +0200

    Added full support for Arch Linux and macOS as it is now in the current state for CLI alpha build

---
+++ build/CMakeFiles/HalfLifeModelViewer.dir/DependInfo.cmake
@@ -0,0 +1,36 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/karlosiric/Documents/SublimeText Programming/C_Projects/ModelViewer/src/graphics/camera.c" "CMakeFiles/HalfLifeModelViewer.dir/src/graphics/camera.c.o" "gcc" "CMakeFiles/HalfLifeModelViewer.dir/src/graphics/camera.c.o.d"
+  "/Users/karlosiric/Documents/SublimeText Programming/C_Projects/ModelViewer/src/graphics/renderer.c" "CMakeFiles/HalfLifeModelViewer.dir/src/graphics/renderer.c.o" "gcc" "CMakeFiles/HalfLifeModelViewer.dir/src/graphics/renderer.c.o.d"
+  "/Users/karlosiric/Documents/SublimeText Programming/C_Projects/ModelViewer/src/graphics/textures.c" "CMakeFiles/HalfLifeModelViewer.dir/src/graphics/textures.c.o" "gcc" "CMakeFiles/HalfLifeModelViewer.dir/src/graphics/textures.c.o.d"
+  "/Users/karlosiric/Documents/SublimeText Programming/C_Projects/ModelViewer/src/main.c" "CMakeFiles/HalfLifeModelViewer.dir/src/main.c.o" "gcc" "CMakeFiles/HalfLifeModelViewer.dir/src/main.c.o.d"
+  "/Users/karlosiric/Documents/SublimeText Programming/C_Projects/ModelViewer/src/mdl/bodypart_manager.c" "CMakeFiles/HalfLifeModelViewer.dir/src/mdl/bodypart_manager.c.o" "gcc" "CMakeFiles/HalfLifeModelViewer.dir/src/mdl/bodypart_manager.c.o.d"
+  "/Users/karlosiric/Documents/SublimeText Programming/C_Projects/ModelViewer/src/mdl/bone_system.c" "CMakeFiles/HalfLifeModelViewer.dir/src/mdl/bone_system.c.o" "gcc" "CMakeFiles/HalfLifeModelViewer.dir/src/mdl/bone_system.c.o.d"
+  "/Users/karlosiric/Documents/SublimeText Programming/C_Projects/ModelViewer/src/mdl/mdl_animations.c" "CMakeFiles/HalfLifeModelViewer.dir/src/mdl/mdl_animations.c.o" "gcc" "CMakeFiles/HalfLifeModelViewer.dir/src/mdl/mdl_animations.c.o.d"
+  "/Users/karlosiric/Documents/SublimeText Programming/C_Projects/ModelViewer/src/mdl/mdl_info.c" "CMakeFiles/HalfLifeModelViewer.dir/src/mdl/mdl_info.c.o" "gcc" "CMakeFiles/HalfLifeModelViewer.dir/src/mdl/mdl_info.c.o.d"
+  "/Users/karlosiric/Documents/SublimeText Programming/C_Projects/ModelViewer/src/mdl/mdl_loader.c" "CMakeFiles/HalfLifeModelViewer.dir/src/mdl/mdl_loader.c.o" "gcc" "CMakeFiles/HalfLifeModelViewer.dir/src/mdl/mdl_loader.c.o.d"
+  "/Users/karlosiric/Documents/SublimeText Programming/C_Projects/ModelViewer/src/mdl/mdl_report.c" "CMakeFiles/HalfLifeModelViewer.dir/src/mdl/mdl_report.c.o" "gcc" "CMakeFiles/HalfLifeModelViewer.dir/src/mdl/mdl_report.c.o.d"
+  "/Users/karlosiric/Documents/SublimeText Programming/C_Projects/ModelViewer/src/utils/args.c" "CMakeFiles/HalfLifeModelViewer.dir/src/utils/args.c.o" "gcc" "CMakeFiles/HalfLifeModelViewer.dir/src/utils/args.c.o.d"
+  "/Users/karlosiric/Documents/SublimeText Programming/C_Projects/ModelViewer/src/utils/logger.c" "CMakeFiles/HalfLifeModelViewer.dir/src/utils/logger.c.o" "gcc" "CMakeFiles/HalfLifeModelViewer.dir/src/utils/logger.c.o.d"
+  "/Users/karlosiric/Documents/SublimeText Programming/C_Projects/ModelViewer/src/utils/mdl_messages.c" "CMakeFiles/HalfLifeModelViewer.dir/src/utils/mdl_messages.c.o" "gcc" "CMakeFiles/HalfLifeModelViewer.dir/src/utils/mdl_messages.c.o.d"
+  "/Users/karlosiric/Documents/SublimeText Programming/C_Projects/ModelViewer/src/utils/utils.c" "CMakeFiles/HalfLifeModelViewer.dir/src/utils/utils.c.o" "gcc" "CMakeFiles/HalfLifeModelViewer.dir/src/utils/utils.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/HalfLifeModelViewer.dir/src/graphics/camera.c.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/HalfLifeModelViewer.dir/src/graphics/camera.c.o: \
+  /Users/karlosiric/Documents/SublimeText\ Programming/C_Projects/ModelViewer/src/graphics/camera.c \
+  /Users/karlosiric/Documents/SublimeText\ Programming/C_Projects/ModelViewer/src/graphics/camera.h
