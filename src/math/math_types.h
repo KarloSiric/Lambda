9d6f910dba480a7b2e31d57b5ad0008bebbf2b53
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-10-24 19:09:50 +0200

    Added for now only vector3 math functions as wrappers for cglm library functions

--- src/math/math_types.h
+++ src/math/math_types.h
@@ -2,11 +2,13 @@
 #define MATH_TYPES_H 
 
 #include <cglm/cglm.h>
+#include <cglm/struct.h>
 #include <cglm/vec3.h>
 #include <cglm/mat3x4.h>
 #include <cglm/mat4.h>
 #include <stdbool.h>
 #include <math.h>
+#include <stdalign.h>
 
 
 #define MATH_PI             3.14159265358979323846f
@@ -26,9 +28,10 @@ typedef versor              math_quat_t;
 typedef mat4                math_mat4_t;
 typedef mat3                math_mat3_t;
 
+
 typedef struct {
     
-    float mat[3][4];
+    alignas(16) float mat[3][4];
     
 } math_mat3x4_t;
 
