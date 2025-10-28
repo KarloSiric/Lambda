8f0b62e9016619ed6808a41ecccd5245f673429d
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-10-28 16:34:48 +0100

    Adding more utilities almost finished with math library and then we move on to refactoring the code as whole in terms of renderer and other things

--- src/math/math_utils.c
+++ src/math/math_utils.c
@@ -1,16 +1,79 @@
-/*======================================================================
-   File: math_utils.c
-   Project: ModelViewer
-   Author: karlosiric <email@example.com>
-   Created: 2025-10-24 18:09:40
-   Last Modified by: karlosiric
-   Last Modified: 2025-10-26 14:18:16
-   ---------------------------------------------------------------------
-   Description:
-       
-   ---------------------------------------------------------------------
-   License: 
-   Company: 
-   Version: 0.1.0
- ======================================================================
-                                                                       */
+/*
+ * ═══════════════════════════════════════════════════════════════════════════
+ *   Half-Life Model Viewer
+ * ═══════════════════════════════════════════════════════════════════════════
+ *
+ *   Copyright (c) 1996-2002, Valve LLC. All rights reserved.
+ *
+ *   This product contains software technology licensed from Id
+ *   Software, Inc. ("Id Technology"). Id Technology (c) 1996 Id Software, Inc.
+ *   All Rights Reserved.
+ *
+ *   Use, distribution, and modification of this source code and/or resulting
+ *   object code is restricted to non-commercial enhancements to products from
+ *   Valve LLC. All other use, distribution, or modification is prohibited
+ *   without written permission from Valve LLC.
+ *
+ * ───────────────────────────────────────────────────────────────────────────
+ *   Author: Karlo Siric
+ *   Purpose: Math utility functions for math library
+ * ═══════════════════════════════════════════════════════════════════════════
+ */
+
+#include "math_types.h"
+
+
+float Math_Clamp( float value, float min, float max ) {
+    
+    float temp;
+    
+    
+    // 7, 5 -> 7 > 5 
+    
+    if ( min > max ) {
+        temp = min;
+        min = max;
+        max = temp;
+    }
+    
+    if ( value < min ) {
+        value = min;
+    }
+    if ( value > max ) {
+        value = max;
+    }
+     
+    return value;    
+}
+
+float Math_Min( float a, float b ) {
+    
+    float min_value;
+    
+    if ( a < b ) {
+        min_value = a;
+    } else {
+        min_value = b;
+    }
+
+    return min_value;
+    
+}
+
+float Math_Max( float a, float b ) {
+    
+    float max_value;
+    if ( a > b ) {
+        max_value = a;
+    } else {
+        max_value = b;
+    }
+    
+    return max_value; 
+}
+
+float Math_Lerp( float a, float b, float t ) {
+    
+    return ( a + ( t * ( b - a ) ) );
+    
+}
