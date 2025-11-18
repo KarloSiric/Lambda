a870bf90058e803182d1e04f71ee924e90f5dbf3
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-11-18 12:34:31 +0100

    Adding functions for attachments

--- src/mdl/mdl_attachments.h
+++ src/mdl/mdl_attachments.h
@@ -9,6 +9,12 @@
 #pragma once
 
 
+// @Note(Karlo): First we need to be able to find the attachment by its name
+int mdl_attachment_find( const studiohdr_t *header, const unsigned char *data, const char *name );
+
+// @Note(Karlo): We get the attachment definition
+const mstudioattachment_t *mdl_attachment_get_info( const studiohdr_t *header, const unsigned char *data, int index );
+
 
 
 
