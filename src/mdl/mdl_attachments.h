5dbc54248343d4cf11cecaf59482dfbf0c65eb2c
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-11-18 12:58:40 +0100

    Implemented all of the mdl_attachments header file now we need to implement the c file for them as well

--- src/mdl/mdl_attachments.h
+++ src/mdl/mdl_attachments.h
@@ -8,6 +8,10 @@
 
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 
 // @Note(Karlo): First we need to be able to find the attachment by its name
 int mdl_attachment_find( const studiohdr_t *header, const unsigned char *data, const char *name );
@@ -17,6 +21,10 @@ const mstudioattachment_t *mdl_attachment_get_info( const studiohdr_t *header, c
 
 // @Note(Karlo): We need to calculate the world position of the attachment
 void mdl_attachment_get_world_position( const mstudioattachment_t *attachment, const math_mat4_t *bone_matrices, math_vec3_t out_position );
+
+void mdl_attachment_get_world_orientation( const mstudioattachment_t *attachment, const math_mat4_t *bone_matrices, math_vec3_t out_forward, math_vec3_t out_right, math_vec3_t out_up );
+
+void mdl_attachment_dump( const studiohdr_t *header, const unsigned char *data );
 
 
 #endif
