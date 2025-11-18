460dfa5f86f1a869decad4e97460f49b6a0f62ce
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-11-18 12:40:54 +0100

    Added declaration for the world position realtive to the bone and output that to the world position, from local->realtive to bone->world

--- src/mdl/mdl_attachments.h
+++ src/mdl/mdl_attachments.h
@@ -15,7 +15,8 @@ int mdl_attachment_find( const studiohdr_t *header, const unsigned char *data, c
 // @Note(Karlo): We get the attachment definition
 const mstudioattachment_t *mdl_attachment_get_info( const studiohdr_t *header, const unsigned char *data, int index );
 
-
+// @Note(Karlo): We need to calculate the world position of the attachment
+void mdl_attachment_get_world_position( const mstudioattachment_t *attachment, const math_mat4_t *bone_matrices, math_vec3_t out_position );
 
 
 #endif
