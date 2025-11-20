0be8e8c3fdcc5fbf69302acfb002b1e7bec6dc91
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-11-20 17:59:22 +0100

    Started adding the audio engine system using the miniaudio library for this project

--- src/mdl/mdl_audio.c
+++ src/mdl/mdl_audio.c
@@ -28,12 +28,78 @@
 #include <sys/stat.h>
 
 #define MAX_SEARCH_PATHS                 8
-#define MINIAUDIO IMPLEMENTATION
+#define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio.h"
 
 // @Note: Adding some global scope variables needed for work
 static ma_engine g_audio_engine;
 static bool g_audio_initialized = false;
+
+
+static char g_sound_search_paths[MAX_SEARCH_PATHS][512];
+static int g_num_search_paths = 0;
+
+
+static bool g_sounds_available = false;
+
+
+bool mdl_audio_init( void ) {
+    
+    if ( g_audio_initialized ) {
+        return true;
+    }
+    
+    ma_result result = ma_engine_init( NULL, &g_audio_engine );
+    
+    if ( result != MA_SUCCESS ) {
+        fprintf(stderr, "[AUDIO] Failed to initialize audio engine.\n");
+        return false;
+    }
+    
+    g_audio_initialized = true;
+    
+    printf( "[AUDIO] Audio engine initialized successfully!\n" );
+    
+    return true;
+}
+
+
+void mdl_audio_shutdown( void ) {
+    
+    if ( !g_audio_initialized ) {
+        return ;
+    }
+    
+    ma_engine_uninit( &g_audio_engine );
+    g_audio_initialized = false;
+    g_num_search_paths = 0;
+    g_sounds_available = false;
+    
+    printf( "[AUDIO] Audio Engine shuwtdown.\n");
+    
+}
+
+// @Note: Helper function for finding out where the 
+static bool mdl_audio_directory_exists( const char *path ) {
+    
+    struct stat info;
+    
+    if ( stat(path, &info) != 0 ) {
+        return false;
+    }
+    
+    return ( info.st_mode & S_IFDIR ) != 0; 
+}
+
+// @Note: Need to extract things so -> /path/to/model.mdl -> /path/to
+static void mdl_audio_extract_directory( const char *path, char *out_path, size_t out_size ) {
+    
+    
+    
+}
+
+
+
 
 
 
