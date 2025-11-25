dd238976b6de000bb13924dcfca1baef95c3eead
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-11-25 21:23:39 +0100

    Cmake fucking error I am done... gone work on it in a few days sick of this cmake shit

--- src/editor/MainWindow.h
+++ src/editor/MainWindow.h
@@ -1,30 +1,46 @@
 #ifndef MAINWINDOW_H
-#define MAINWINDOW_H 
-
-#pragma once
+#define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QDockWidget>
+#include <QWidget>
+#include <QOpenGLWidget>
 
+#define MW_WIDTH 1600
+#define MW_HEIGHT 900
 
-#define MW_WIDTH               1600
-#define MW_HEIGHT              900
-
-class MainWindow : public QMainWindow 
+class MainWindow : public QMainWindow
 {
     Q_OBJECT
-    
+
 public:
-    
-    explicit MainWindow( QWidget *parent = nullptr );
-    
+    explicit MainWindow(QWidget *parent = nullptr);
+
 private:
-    
-    void setupMenus( );
-    void setupToolbars( );
-    void setupLeftToolbox( );
-    void setupRightInspector( );
-    void setupViewports( );
-    
+    void setupMenus();
+    void setupToolbars();
+    void setupLeftToolbox();
+    void setupRightInspector();
+    void setupViewports();
+
+    void createFileMenu();
+    void createEditMenu();
+    void createViewMenu();
+    void createToolsMenu();
+    void createDebugMenu();
+    void createWindowMenu();
+    void createHelpMenu();
+
+    void setTheme();
+
+    QDockWidget *leftDock;
+    QDockWidget *rightDock;
+    QDockWidget *bottomDock;
+
+    QWidget *viewportContainer;
+
+    void createDocks();
+    void createViewportContainer();
 };
 
 #endif
