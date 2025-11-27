5b7355c989d2bb10365d004b5054f2ce5c44925c
ksiric <144100868+KarloSiric@users.noreply.github.com>
2025-11-27 15:57:11 +0100

    working on the GUI, things are taking its time and toll but I will get there little by littel so still need to design things properly

--- src/editor/MainWindow.h
+++ src/editor/MainWindow.h
@@ -5,9 +5,17 @@
 #include <QDockWidget>
 #include <QWidget>
 #include <QOpenGLWidget>
+#include <QtWidgets/qmainwindow.h>
 
-#define MW_WIDTH 1600
-#define MW_HEIGHT 900
+#define MW_WIDTH                    1920
+#define MW_HEIGHT                   1080
+
+#define MW_CONSOLE_MIN_HEIGHT       350
+#define MW_CONSOLE_MAX_HEIGHT       1000
+
+#define MW_RIGHT_DOCK_MIN_WIDTH     350 
+#define MW_RIGHT_DOCK_MAX_WIDTH     1000 
+
 
 class MainWindow : public QMainWindow
 {
@@ -36,6 +44,7 @@ private:
     QDockWidget *leftDock;
     QDockWidget *rightDock;
     QDockWidget *bottomDock;
+    QStatusBar *statusBar;
 
     QWidget *viewportContainer;
 
